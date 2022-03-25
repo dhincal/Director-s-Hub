c8184b5660883558dc5a8f2473bae99c0d6cf197
Deniz <denizhincal05@gmail.com>
2022-03-25 10:50:02 +0300

    Add Movie Listing
    Now all the Movie's are listed by GET method in movies router

--- routes/movie.js
+++ routes/movie.js
@@ -3,9 +3,18 @@ var router = express.Router();
 
 const Movie = require('../models/Movie');
 
+router.get('/', (req, res) => {
+  const promise = Movie.find({});
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  })
+})
+
 router.post('/', (req, res, next) => {
   
-  const movie = new Movie (req.body);
+  const movie = new Movie(req.body);
 
   const promise = movie.save();
   promise.then((data) => {
